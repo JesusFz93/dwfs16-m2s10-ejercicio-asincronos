4524f3e0fc97d57012d438f72f5782b0abc96a0a
Jesus <jesus.fdz93@gmail.com>
2023-04-01 13:23:26 -0500

    promesas

--- js/promesas.js
+++ js/promesas.js
@@ -1,12 +1,42 @@
 // Para crear promesas
 const promesa1 = new Promise((resolve, reject) => {
   setTimeout(() => {
-    let correcto = false;
+    const usuarios = [
+      {
+        id: 1,
+        nombre: "Pablo",
+        email: "p@gmail.com",
+      },
+      {
+        id: 2,
+        nombre: "Jose",
+        email: "j@gmail.com",
+      },
+      {
+        id: 3,
+        nombre: "Fernando",
+        email: "f@gmail.com",
+      },
+    ];
 
-    if (correcto) {
-      resolve("Promesa cumplida");
+    // const edad = 17;
+
+    // let correcto = false;
+
+    if (usuarios.length > 0) {
+      const resp = {
+        msg: "Usuarios obtenidos",
+        data: usuarios,
+      };
+
+      resolve(resp);
     } else {
-      reject("Promesa rechazada");
+      const resp = {
+        msg: "Lista vacia",
+        data: [],
+      };
+
+      reject(resp);
     }
   }, 3000);
 });
@@ -18,4 +48,7 @@ promesa1
   })
   .catch((error) => {
     console.log(error);
+  })
+  .finally(() => {
+    console.log("Se ejecuto al final de la promesa");
   });
