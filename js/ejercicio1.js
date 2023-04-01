097bac0bb125c6f5848dde94df0fb266c0a2ed9a
Jesus <jesus.fdz93@gmail.com>
2023-04-01 12:29:22 -0500

    callbacks

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -4,3 +4,19 @@
 // // En la función callback, recupera el resultado e imprímelo en consola.
 // // Ahora bien, manda llamar a tu función saludar.
 
+const saludar = (nombre, callback) => {
+  let nombreMayusculas = nombre.toUpperCase();
+
+  setTimeout(() => {
+    callback(nombreMayusculas);
+  }, 4000);
+};
+
+const imprimeNombre = (nombreEnMayusculas) => {
+  console.log(nombreEnMayusculas);
+};
+
+saludar("fernandez", imprimeNombre);
+
+console.log("final");
+// imprimeNombre("fernandez");
